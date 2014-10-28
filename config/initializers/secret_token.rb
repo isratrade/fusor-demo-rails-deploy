78f8cd7840fe1b76e2ed7cb79c9cfdb5e7682ce0
Joseph Magen <joseph@isratrade.co.il>
2014-10-28 09:12:49 +0200

    initial rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhci2::Application.config.secret_token = '60c3f4873978551fb5583a817333ef6cbf3d28acfde85879daea068c25d85400ded6c9e0cbd0aaecfacb9ada059cf6c9936bc3c776d46b42a15e4525bd7588b7'
